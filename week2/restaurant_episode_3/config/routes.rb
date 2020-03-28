88e52c06b8b8d6cd38a737f55737510f9ef2d7b4
Deanout <dddehart@oakland.edu>
2020-03-28 02:01:23 -0400

    Week 2 Episode 3 commit

---
+++ week2/restaurant_episode_3/config/routes.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  resources :categories
+  get 'menu', to: 'menu#index'
+  devise_for :users
+  resources :products
+  root 'pages#home'
+  get 'catering', to: 'pages#catering'
+  get 'info', to: 'pages#info'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ week2/restaurant_episode_3/test/controllers/categories_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @category = categories(:one)
+  end
+
+  test "should get index" do
+    get categories_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_category_url
+    assert_response :success
+  end
+
+  test "should create category" do
+    assert_difference('Category.count') do
+      post categories_url, params: { category: { description: @category.description, name: @category.name } }
+    end
+
+    assert_redirected_to category_url(Category.last)
+  end
+
+  test "should show category" do
+    get category_url(@category)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_category_url(@category)
+    assert_response :success
+  end
+
+  test "should update category" do
+    patch category_url(@category), params: { category: { description: @category.description, name: @category.name } }
+    assert_redirected_to category_url(@category)
+  end
+
+  test "should destroy category" do
+    assert_difference('Category.count', -1) do
+      delete category_url(@category)
+    end
+
+    assert_redirected_to categories_url
+  end
+end
