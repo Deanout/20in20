d3a23a88992129eea40e8b4d07f024a4fbd14f8d
Deanout <dddehart@oakland.edu>
2020-04-04 10:31:11 -0400

    Week 3 Episode 1 final commit

---
+++ week3/portfolio_1_old/test/controllers/pages_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get pages_index_url
+    assert_response :success
+  end
+
+end
--- week3/portfolio_episode_1/config/environments/production.rb
+++ week3/portfolio_episode_1/config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "week3demo_production"
+  # config.active_job.queue_name_prefix = "portfolio_episode_1_production"
 
   config.action_mailer.perform_caching = false
 
