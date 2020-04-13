2541fe9d8b25e7c743e3683a90a7a7bf10f52426
Deanout <dddehart@oakland.edu>
2020-04-13 06:31:05 -0400

    Add final JS Application

--- week4/api_task_user/application.js
+++ week4/api_task_user/application.js
@@ -1,3 +1,5 @@
+var elementAdditionPosition = "afterbegin";
+
 function getTasks() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -8,8 +10,8 @@ function getTasks() {
 
       for (var i = 0; i < responses.length; i++) {
         container.insertAdjacentHTML(
-          "beforeend",
-          "[" + responses[i].id + "]: " + responses[i].task + "<br />"
+          elementAdditionPosition,
+          createResponse(responses[i])
         );
       }
     }
@@ -18,4 +20,42 @@ function getTasks() {
   xhttp.send();
 }
 
+window.sendForm = function (event) {
+  event.preventDefault();
+  var xhttp = new this.XMLHttpRequest();
+  xhttp.open("POST", "http://localhost:3001/todos", true);
+  xhttp.onload = function (event) {
+    let container = document.getElementById("container");
+    let response = JSON.parse(event.target.response);
+
+    container.insertAdjacentHTML(
+      elementAdditionPosition,
+      createResponse(response)
+    );
+  };
+  var formData = new this.FormData(document.getElementById("todo_form"));
+  xhttp.send(formData);
+  document.getElementById("task_input").value = "";
+};
+
+function createResponse(response) {
+  return (
+    `<div class="task" id="${response.id}" onclick="deleteResponse('${response.id}')">` +
+    "[" +
+    response.id +
+    "]: " +
+    response.task +
+    "<br />" +
+    "</div>"
+  );
+}
+function deleteResponse(response_id) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("DELETE", `http://localhost:3001/todos/${response_id}`, true);
+  xhttp.onload = function () {
+    let target = document.getElementById(response_id);
+    target.parentNode.removeChild(target);
+  };
+  xhttp.send(null);
+}
 getTasks();
