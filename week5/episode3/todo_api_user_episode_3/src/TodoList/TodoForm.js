a15cd1386a17ac6b463e6cf184c342ecd0778750
Deanout <dddehart@oakland.edu>
2020-04-15 15:12:22 -0400

    Week 5 Episode 3? Commit

--- week5/episode3/todo_api_user_episode_3/src/TodoList/TodoForm.js
+++ week5/episode3/todo_api_user_episode_3/src/TodoList/TodoForm.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
+import React, { Component } from "react"
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 import Grid from "@material-ui/core/Grid"
-import Button from "@material-ui/core/Button"
-
 
 class TodoForm extends Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             api_url: props.api_url,
             task: "",
-            body: "",
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleTaskChange = this.handleTaskChange.bind(this);
@@ -21,6 +19,15 @@ class TodoForm extends Component {
         this.formSubmit(event.target);
     }
 
+    async formSubmit(formData) {
+        var data = new FormData(formData);
+        await fetch(this.state.api_url, {
+            method: "POST",
+            mode: "cors",
+            body: data
+        }).then(response => response.json())
+            .then(response => this.props.updateTodoList(response))
+    }
 
     handleTaskChange(event) {
         this.setState({
@@ -28,20 +35,8 @@ class TodoForm extends Component {
         })
     }
 
-    async formSubmit(formData) {
-        var data = new FormData(formData);
-        var response = await fetch(this.state.api_url, {
-            method: 'POST',
-            mode: 'cors',
-            body: data
-        }).then(response => response.json()
-            .then(response => this.props.updateTodoList(response)));
-
-    }
-
     render() {
         return (
-
             <Grid container>
                 <Grid item xs></Grid>
                 <Grid item xs={10}>
@@ -51,8 +46,9 @@ class TodoForm extends Component {
                         autoComplete="off">
                         <Grid container>
                             <Grid item xs={10}>
-                                <TextField id="task_input"
-                                    label="Task Name"
+                                <TextField
+                                    id="task_input"
+                                    label="Task Description"
                                     variant="outlined"
                                     type="text"
                                     name="todo[task]"
@@ -63,15 +59,14 @@ class TodoForm extends Component {
                                 <Button variant="contained"
                                     color="primary"
                                     type="submit"
-                                    style={{ height: "100%" }}>Submit</Button>
+                                    style={{ height: "100%" }}>Add Task</Button>
                             </Grid>
                         </Grid>
                     </form>
                 </Grid>
                 <Grid item xs></Grid>
             </Grid>
-        );
+        )
     }
 }
-
 export default TodoForm;
--- week5/episode3/todo_api_user_episode_3/src/TodoList/TodoItem.js
+++ week5/episode3/todo_api_user_episode_3/src/TodoList/TodoItem.js
@@ -1,23 +1,22 @@
-import React, { Component } from "react";
-import Button from "@material-ui/core/Button"
-import DeleteIcon from '@material-ui/icons/Delete';
+import React from "react";
 import Grid from "@material-ui/core/Grid"
 import Paper from "@material-ui/core/Paper"
 import { makeStyles } from "@material-ui/core/styles"
+import Button from "@material-ui/core/Button"
+import DeleteIcon from "@material-ui/icons/Delete"
 
 const useStyles = makeStyles({
     root: {
         height: "3em",
         padding: "2em",
-        margin: "1em",
-    },
-});
-
+        margin: "1em"
+    }
+})
 
 export default function TodoItem(props) {
     const classes = useStyles();
     function handleDelete() {
-        props.deleteItem(props.item);
+        props.deleteItem(props.item)
     }
     return (
         <Grid container spacing={0}>
@@ -30,12 +29,11 @@ export default function TodoItem(props) {
                         color="secondary"
                         size="small"
                         startIcon={<DeleteIcon />}
-                        onClick={handleDelete}
-                    >
+                        onClick={handleDelete}>
                         Delete
                     </Button>
                 </Paper>
             </Grid>
         </Grid>
-    );
+    )
 }
