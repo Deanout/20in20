a953aea87ac5834da6e070591f48e2a42afa6a1b
Ahmet Kaptan <38915226+codescaptain@users.noreply.github.com>
2022-01-02 23:11:20 +0300

    Add respond for turbo_stream (#156)
    if we don't type respond, we get the 406 error code.

--- week18/hotwire-todo/app/controllers/completed_controller.rb
+++ week18/hotwire-todo/app/controllers/completed_controller.rb
@@ -3,5 +3,10 @@ class CompletedController < ApplicationController
     @todo = Todo.find(params[:id])
     @todo.completed = !@todo.completed
     @todo.save
+    # if we don't type respond, we get the 406 error code. 
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace(@todo) }
+      format.html         { redirect_to todos_url }
+    end
   end
 end
